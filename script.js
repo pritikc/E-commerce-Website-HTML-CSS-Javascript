5312d0c7261813eee17b4539ed99895617173811
Priti Chauhan <priti.chauhan1518@gmail.com>
2023-02-22 16:05:52 -0800

    sic

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// hamburger_menu 
+// hamburger_menu - bar
 const bar = document.getElementById("bar");
 const nav = document.getElementById("navBar");
 const close = document.getElementById("close");
@@ -13,3 +13,19 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+// singleproduct -> Show amall to big image
+var main_Img = document.getElementById("main_Img");
+var sml_Img = document.getElementsByClassName("sml_Img");
+
+sml_Img[0].addEventListener('click', () => {
+    main_Img.src = sml_Img[0].src;
+})
+sml_Img[1].addEventListener('click', () => {
+    main_Img.src = sml_Img[1].src;
+})
+sml_Img[2].addEventListener('click', () => {
+    main_Img.src = sml_Img[2].src;
+})
+sml_Img[3].addEventListener('click', () => {
+    main_Img.src = sml_Img[3].src;
+})
